35b8e1b895ce4f910f06d98c209d99938aab7331
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-29 20:42:05 +0100

    add milestone 3 filters

--- functions.php
+++ functions.php
@@ -1,36 +1,47 @@
 <?php
+/* Milestone 3 (BONUS)
+1 Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli.
+2- Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme). 3-Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. */
+
+//var_dump($_GET['repetition']);
 
 function pass_generator($max_length)
 {
+    if ($max_length == null) {
+        echo 'Non hai inserito la lunghezza della password!';
+    }
+
     $permitted_chars = [
         'numbers' => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,],
         'letters' => ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',],
-        'symbols' => ['!', '?', '*', '#', '£', '$', '%']
+        'simbols' => ['!', '?', '*', '#', '£', '$', '%']
     ];
 
     $random_password = '';
 
-    if (isset($_GET['letters']) == 'on') {
+    if ($_GET['letters'] == 'letters') { // filter by letters
         for ($i = 0; $i < $max_length; $i++) {
             $random_char = $permitted_chars['letters'][mt_rand(0, (count($permitted_chars['letters']) - 1))];
 
             $random_password .= $random_char;
         }
     }
-    if (isset($_GET['numbers']) == 'on') {
+    if ($_GET['numbers'] == 'numbers') { // filter by numbers
         for ($i = 0; $i < $max_length; $i++) {
             $random_char = $permitted_chars['numbers'][mt_rand(0, (count($permitted_chars['numbers']) - 1))];
 
             $random_password .= $random_char;
         }
     }
-    if (isset($_GET['symbols']) == 'on') {
+    if ($_GET['simbols'] == 'simbols') { // filter by smbols
         for ($i = 0; $i < $max_length; $i++) {
-            $random_char = $permitted_chars['symbols'][mt_rand(0, (count($permitted_chars['symbols']) - 1))];
+            $random_char = $permitted_chars['simbols'][mt_rand(0, (count($permitted_chars['simbols']) - 1))];
 
             $random_password .= $random_char;
         }
     }
 
-    return $random_password;
+    $shuffled = str_shuffle($random_password);
+
+    return mb_strimwidth($shuffled, 0, $max_length);
 }
--- index.php
+++ index.php
@@ -2,15 +2,11 @@
 
 include 'functions.php';
 
-if (isset($_GET['password_length'])) {
+if (isset($_GET)) {
     echo pass_generator(($_GET['password_length']));
 }
 
-/* Milestone 3 (BONUS)
-1 Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli.
-2- Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme). 3-Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. */
-
-//var_dump($_GET['repetition']);
+var_dump($_GET['letters']);
 
 ?>
 
@@ -33,7 +29,7 @@ if (isset($_GET['password_length'])) {
     <form action="index.php" method="get">
         <div class="mb-3">
             <label for="password_length" class="form-label">Lunghezza password</label>
-            <input type="number" class="form-control" name="password_length" id="password_length" aria-describedby="helpId" placeholder="type a password">
+            <input type="number" class="form-control" name="password_length" id="password_length" placeholder="type a password">
         </div>
         <div class="mb-3">
             <div>Consenti la riproduzione di uno o più caratteri:</div>
@@ -44,11 +40,11 @@ if (isset($_GET['password_length'])) {
         </div>
         <div class="mb-3 d-flex">
             <label for="letters" class="form-label">Lettere</label>
-            <input type="checkbox" class="form-control" name="letters" id="letters" aria-describedby="helpId" placeholder="type a password">
+            <input type="checkbox" class="form-control" name="letters" id="letters" value="letters">
             <label for="numbers" class="form-label">Numeri</label>
-            <input type="checkbox" class="form-control" name="numbers" id="numbers" aria-describedby="helpId" placeholder="type a password">
-            <label for="symbols" class="form-label">Simboli</label>
-            <input type="checkbox" class="form-control" name="symbols" id="symbols" aria-describedby="helpId" placeholder="type a password">
+            <input type="checkbox" class="form-control" name="numbers" id="numbers" value="numbers">
+            <label for="simbols" class="form-label">Simboli</label>
+            <input type="checkbox" class="form-control" name="simbols" id="simbols" value="simbols">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
         <button type="reset" class="btn btn-primary">Reset</button>
